f383bc7c853086753eca11c0017cfe2af8b7239b
formation <formateur@mail.com>
2018-01-08 12:21:39 +0100

    Ajout d'un bouton dans MainActivity pour afficher FormActivity

--- app/src/main/java/sm/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/MainActivity.java
@@ -1,7 +1,9 @@
 package sm.fr.localsqlapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Lancement de l'activité formulaire au clic sur un bouton
+     * @param view
+     */
+    public void onAddContact(View view) {
+        if(view == findViewById(R.id.buttonAddContact)){
+            Intent FormIntent = new Intent(this, FormActivity.class);
+            startActivity(FormIntent);
+        }
     }
 }

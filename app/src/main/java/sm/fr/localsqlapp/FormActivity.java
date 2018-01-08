e505f2dd4de1d2a4909ee008c7fd5b5ee6edfbb2
formation <formateur@mail.com>
2018-01-08 15:15:11 +0100

    Insertion d'un contact dans la base SQLite

--- app/src/main/java/sm/fr/localsqlapp/FormActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/FormActivity.java
@@ -1,9 +1,14 @@
 package sm.fr.localsqlapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import fr.sm.database.DatabaseHandler;
 
@@ -18,6 +23,28 @@ public class FormActivity extends AppCompatActivity {
     public void onValid(View v){
         Button clickedButton = (Button) v;
 
+        //Récupération de la saisie de l'utilisateur
+        String name = ((EditText) findViewById(R.id.editTextNom)).getText().toString();
+        String firstName = ((EditText) findViewById(R.id.editTextPrenom)).getText().toString();
+        String email = ((EditText) findViewById(R.id.editTextEmail)).getText().toString();
+
+        //Instanciation de la connexion à la base de données
         DatabaseHandler db = new DatabaseHandler(this);
+
+        //Définition des données à insérer
+        ContentValues insertValues = new ContentValues();
+        insertValues.put("first_name", firstName);
+        insertValues.put("name", name);
+        insertValues.put("email", email);
+
+        //Insertion des données
+        try {
+            db.getWritableDatabase().insert("contacts", null,insertValues);
+            Toast.makeText(this, "Insertion OK", Toast.LENGTH_LONG).show();
+
+        } catch (SQLiteException ex){
+            Log.e("SQL EXCEPTION", ex.getMessage());
+        }
+
     }
 }
